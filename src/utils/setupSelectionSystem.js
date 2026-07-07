3c158db0cd631e571783427661fcfa22040157f5
adnan wahab <shel.bernstein123@gmail.com>
2026-07-07 11:27:27 -0500

    simplify

--- src/utils/setupSelectionSystem.js
+++ src/utils/setupSelectionSystem.js
@@ -1,33 +1,41 @@
-import { SelectionSystem } from '../selectionSystem.js';
+import { SelectionSystem } from "../modules/graphics/selectionSystem.js";
 
 export function setupSelectionSystem(scene, camera, canvas, cleanupFunctions) {
-    const selectionSystem = new SelectionSystem(scene, camera, canvas);
-    let allowSelection = true;
-    let mouseDownPos = null;
+  const selectionSystem = new SelectionSystem(scene, camera, canvas);
+  let allowSelection = true;
+  let mouseDownPos = null;
 
-    const onPointerDown = (e) => {
-        if (e.button === 0) { mouseDownPos = { x: e.clientX, y: e.clientY }; allowSelection = true; }
-    };
-    const onPointerMove = (e) => {
-        if (mouseDownPos && e.buttons === 1) {
-            if (Math.abs(e.clientX - mouseDownPos.x) > 5 || Math.abs(e.clientY - mouseDownPos.y) > 5) allowSelection = false;
-        }
-    };
-    const onPointerUp = (e) => {
-        if (e.button === 0 && allowSelection && mouseDownPos) setTimeout(() => selectionSystem.handleClick(e), 10);
-        mouseDownPos = null;
-        allowSelection = true;
-    };
+  const onPointerDown = (e) => {
+    if (e.button === 0) {
+      mouseDownPos = { x: e.clientX, y: e.clientY };
+      allowSelection = true;
+    }
+  };
+  const onPointerMove = (e) => {
+    if (mouseDownPos && e.buttons === 1) {
+      if (
+        Math.abs(e.clientX - mouseDownPos.x) > 5 ||
+        Math.abs(e.clientY - mouseDownPos.y) > 5
+      )
+        allowSelection = false;
+    }
+  };
+  const onPointerUp = (e) => {
+    if (e.button === 0 && allowSelection && mouseDownPos)
+      setTimeout(() => selectionSystem.handleClick(e), 10);
+    mouseDownPos = null;
+    allowSelection = true;
+  };
 
-    canvas.addEventListener('pointerdown', onPointerDown);
-    canvas.addEventListener('pointermove', onPointerMove);
-    canvas.addEventListener('pointerup', onPointerUp);
-    cleanupFunctions.push(() => {
-        canvas.removeEventListener('pointerdown', onPointerDown);
-        canvas.removeEventListener('pointermove', onPointerMove);
-        canvas.removeEventListener('pointerup', onPointerUp);
-        selectionSystem.dispose();
-    });
+  canvas.addEventListener("pointerdown", onPointerDown);
+  canvas.addEventListener("pointermove", onPointerMove);
+  canvas.addEventListener("pointerup", onPointerUp);
+  cleanupFunctions.push(() => {
+    canvas.removeEventListener("pointerdown", onPointerDown);
+    canvas.removeEventListener("pointermove", onPointerMove);
+    canvas.removeEventListener("pointerup", onPointerUp);
+    selectionSystem.dispose();
+  });
 
-    return selectionSystem;
+  return selectionSystem;
 }
